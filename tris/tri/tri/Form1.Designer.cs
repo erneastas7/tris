9f6f168f7a70254c5d248c426411a4498ff9392b
erneastas7 <66021996+erneastas7@users.noreply.github.com>
2021-04-08 22:08:21 +0300

    Add files via upload

---
+++ tris/tri/tri/Form1.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace tri
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Encryptini = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Encryptini
+            // 
+            this.Encryptini.AutoSize = true;
+            this.Encryptini.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Encryptini.Location = new System.Drawing.Point(63, 77);
+            this.Encryptini.Name = "Encryptini";
+            this.Encryptini.Size = new System.Drawing.Size(321, 32);
+            this.Encryptini.TabIndex = 0;
+            this.Encryptini.Text = "Encryptinamas tekstas";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(69, 287);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(315, 70);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Encrypt";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(69, 158);
+            this.textBox1.MinimumSize = new System.Drawing.Size(200, 70);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(315, 22);
+            this.textBox1.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(529, 77);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(322, 32);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Decryptinamas tekstas";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(351, 420);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(255, 32);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Gautas rezultatas";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(206, 478);
+            this.textBox3.MinimumSize = new System.Drawing.Size(200, 60);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(535, 22);
+            this.textBox3.TabIndex = 9;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(535, 158);
+            this.textBox2.MinimumSize = new System.Drawing.Size(200, 70);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(315, 22);
+            this.textBox2.TabIndex = 10;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(535, 288);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(315, 70);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Decrypt";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(535, 196);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(315, 40);
+            this.button3.TabIndex = 12;
+            this.button3.Text = "Decrypt from a file ";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(324, 524);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(324, 40);
+            this.button4.TabIndex = 13;
+            this.button4.Text = "Encrypto atsakymo ikelimas i decryption teksta";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1011, 614);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Encryptini);
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Encryptini;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ tris/tri/tri/Form1.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+using System.IO;
+using System.Diagnostics;
+using System.Globalization;
+using System.Runtime;
+
+namespace tri
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+          
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        RSACryptoServiceProvider RSA = new RSACryptoServiceProvider();
+
+        byte[] plaintext; //textbox1 baitai
+        byte[] encryptedtext; //encryptinto teksto baitai
+       
+        
+  
+        static public byte[] Encryption(byte[] Data, RSAParameters RSAKey, bool DoOAEPPadding)
+        {
+            try
+            {
+                byte[] encryptedData;
+                using (RSACryptoServiceProvider RSA = new RSACryptoServiceProvider())
+                {
+                    RSA.ImportParameters(RSAKey);
+                    encryptedData = RSA.Encrypt(Data, DoOAEPPadding);
+                }
+                return encryptedData;
+            }
+            catch (CryptographicException e)
+            {
+                Console.WriteLine(e.Message);
+                return null;
+            }
+        }
+        static public byte[] Decryption(byte[] Data, RSAParameters RSAKey, bool DoOAEPPadding)
+        {
+            try
+            {
+                byte[] decryptedData;
+                using (RSACryptoServiceProvider RSA = new RSACryptoServiceProvider())
+                {
+                    RSA.ImportParameters(RSAKey);
+                    decryptedData = RSA.Decrypt(Data, DoOAEPPadding);
+                }
+                return decryptedData;
+            }
+            catch (CryptographicException e)
+            {
+                Console.WriteLine(e.ToString());
+                return null;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            var nuskaitymas = string.Empty; 
+            using (OpenFileDialog ofd = new OpenFileDialog())
+            {
+                string path;
+                ofd.Filter = "txt files (*.txt)|*.txt";
+                ofd.RestoreDirectory = true;
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    path = ofd.FileName;
+                    var data = ofd.OpenFile();
+                    using (StreamReader sr = new StreamReader(data))
+                    {
+                        nuskaitymas = sr.ReadToEnd();
+                    }
+                }
+            }
+            try
+            {
+                Encoding encoding = Encoding.GetEncoding("437");
+                byte[] decryptedtex = Decryption(encoding.GetBytes(nuskaitymas),
+                RSA.ExportParameters(true), false);
+                textBox3.Text = encoding.GetString(decryptedtex);
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                Console.WriteLine("Encryption failed.");
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string Path = null;
+
+            try
+            {
+                Encoding encoding = Encoding.GetEncoding("437");
+                plaintext = encoding.GetBytes(textBox1.Text);
+                encryptedtext = Encryption(plaintext, RSA.ExportParameters(false), false);
+                using (var sfd = new SaveFileDialog())
+                {
+                    sfd.Filter = "txt files (*.txt)|*.txt";
+                    sfd.RestoreDirectory = true;
+                    if (sfd.ShowDialog() == DialogResult.OK)
+                    {
+                        Path = sfd.FileName;
+                    }
+                }
+                using (StreamWriter writer = new StreamWriter(Path))
+                {
+                    writer.Write(encoding.GetString(encryptedtext));
+                }
+                textBox3.Text = encoding.GetString(encryptedtext);
+
+            }
+            catch (ArgumentNullException)
+            {
+                Console.WriteLine("Encryption failed.");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                Encoding encoding = Encoding.GetEncoding("437");
+                byte[] decryptedtex = Decryption(encryptedtext, RSA.ExportParameters(true), false);
+                textBox3.Text = encoding.GetString(decryptedtex);
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                Console.WriteLine("Encryption failed.");
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            textBox2.Text = textBox3.Text;
+        }
+    }
+}
